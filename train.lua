07f0c0e199d7af6e432463efc3700b7b499d6027
Peter Nerlich <peter.nerlich+dev@googlemail.com>
2022-01-19 15:44:47 +0100

    draft helping markers

--- train.lua
+++ train.lua
@@ -1,10 +1,12 @@
 
 local advtrains_present = minetest.get_modpath("advtrains") and true or false
 local last_set_by = {}
+local markers = {}
 
 local find_neighbor_blocks -- defined later
 local update_neighbors --defined later
 local recalculate_line_to -- defined later
+local visualize_to_player -- defined later
 local TRAVERSER_LIMIT = 1000
 
 local update_formspec = function(meta)
@@ -91,7 +93,7 @@ minetest.register_node("mapserver:train", {
 		meta:set_string("color", last_color)
 		meta:set_string("rail_pos", "")
 
-		update_neighbors(pos, meta, minetest.is_player(placer) and placer:get_player_name() or nil)
+		update_neighbors(pos, meta, placer, true)
 
 		return mapserver.after_place_node(pos, placer, itemstack, pointed_thing)
 	end,
@@ -101,7 +103,17 @@ minetest.register_node("mapserver:train", {
 
 		-- TODO: why doesn't this work properly?
 
-		update_neighbors(pos, fake_meta, player:get_player_name())
+		update_neighbors(pos, fake_meta, player, true)
+	end,
+
+	on_punch = function(pos, node, puncher, pointed_thing)
+		if minetest.is_player(puncher) and not mapserver.can_interact(pos, puncher) then
+			return
+		end
+
+		local meta = minetest.get_meta(pos)
+
+		update_neighbors(pos, meta, puncher, true)
 	end,
 
 	on_receive_fields = function(pos, formname, fields, sender)
@@ -134,11 +146,11 @@ minetest.register_node("mapserver:train", {
 			last_set_by[lname].station = fields.station
 			last_set_by[lname].index = index
 
-			update_neighbors(pos, meta, name)
+			update_neighbors(pos, meta, sender, true)
 
 		elseif fields.clear_rail_pos then
 			meta:set_string("rail_pos", "")
-			update_neighbors(pos, meta, name)
+			update_neighbors(pos, meta, sender, true)
 
 		elseif fields.set_rail_pos then
 			minetest.chat_send_player(name, "Please punch the nearest rail this train line follows.")
@@ -171,7 +183,7 @@ minetest.register_on_punchnode(function(pos, node, sender, pointed_thing)
 			if node_ok then
 				local meta = minetest.get_meta(blockpos)
 				meta:set_string("rail_pos", minetest.pos_to_string(pos))
-				update_neighbors(blockpos, meta, name)
+				update_neighbors(blockpos, meta, sender, true)
 			else
 				minetest.chat_send_player(name, "This is not a rail! Aborted.")
 			end
@@ -194,10 +206,13 @@ if mapserver.enable_crafting then
 end
 
 
-update_neighbors = function(pos, meta, name)
+update_neighbors = function(pos, meta, player, update_markers)
+	update_markers = update_markers and true or false
 	if meta == nil then
 		meta = minetest.get_meta(pos)
 	end
+	local name = player and minetest.is_player(player) and player:get_player_name() or nil
+
 	local line = meta:get_string("line")
 	local index = tonumber(meta:get_string("index"))
 	local rail_pos = meta:get_string("rail_pos")
@@ -251,11 +266,6 @@ update_neighbors = function(pos, meta, name)
 		meta:set_string("nxt_rail_pos", "")
 
 		meta:set_string("linepath_from_prv", "")
-	end
-
-	if line == "" then
-		update_formspec(meta)
-		return
 	end
 
 	-- update or add us
@@ -363,6 +373,11 @@ update_neighbors = function(pos, meta, name)
 		end
 	end
 	update_formspec(meta)
+
+	if update_markers and
+		player and minetest.is_player(player) then
+		visualize_to_player(pos, meta, neighbors, player, true)
+	end
 end
 
 local nroot = function(root, num)
@@ -408,9 +423,18 @@ find_neighbor_blocks = function(pos, meta, name)
 	if meta == nil then
 		meta = minetest.get_meta(pos)
 	end
+	local all = {}
 	local line = meta:get_string("line")
 	local index = tonumber(meta:get_string("index"))
 	local rail_pos = meta:get_string("rail_pos")
+
+	local empty = {nil, nil, {
+		{ pos = pos, meta = meta,
+		line = line, index = index, rail_pos = rail_pos }
+	}}
+	if line == "" then
+		return empty
+	end
 
 	-- the offsets are chosen so that the resulting area is just under the maximum allowable size
 	local areas = {
@@ -423,25 +447,26 @@ find_neighbor_blocks = function(pos, meta, name)
 		if get_volume(span) > max_nodes then
 			minetest.chat_send_player(name, "Internal Error searching for nearby nodes: Invalid span "..i.." between "..minetest.pos_to_string(span[1]).." and "..minetest.pos_to_string(span[2]).." (volume of "..tostring(get_volume(span))..")")
 			minetest.log("error", "[mapserver_mod][trainlines] Internal Error searching for nearby nodes: Invalid span "..i.." between "..minetest.pos_to_string(span[1]).." and "..minetest.pos_to_string(span[2]).." (volume of "..tostring(get_volume(span))..")")
-			return {}
+			return empty
 		end
 		blocks[i] = minetest.find_nodes_in_area(span[1], span[2], "mapserver:train")
 	end
 	local prv = nil
 	local nxt = nil
-	local meta = nil
+	local m = nil
 
 	for _,span in pairs(blocks) do
 		for _,p in pairs(span) do
-			meta = minetest.get_meta(p)
-			if meta:get_string("line") == line then
-				local idx = tonumber(meta:get_string("index"))
+			m = minetest.get_meta(p)
+			if m:get_string("line") == line then
+				local idx = tonumber(m:get_string("index"))
+				local rail = m:get_string("rail_pos")
 				if idx < index and
 					(prv == nil or idx > prv.index) then
 						prv = {
 							pos = p,
 							index = idx,
-							rail_pos = meta:get_string("rail_pos")
+							rail_pos = rail
 						}
 				end
 				if idx > index and
@@ -449,14 +474,18 @@ find_neighbor_blocks = function(pos, meta, name)
 						nxt = {
 							pos = p,
 							index = idx,
-							rail_pos = meta:get_string("rail_pos")
+							rail_pos = rail
 						}
 				end
+				table.insert(all, {
+					pos = p, meta = m,
+					line = line, index = idx, rail_pos = rail
+				})
 			end
 		end
 	end
 
-	return {prv, nxt}
+	return {prv, nxt, all}
 end
 
 local clone = nil
@@ -555,7 +584,7 @@ recalculate_line_to = function(pos_a, pos_b, meta_a, meta_b)
 						-- only accept conns that turn 90deg at most
 						if normed >= deg45 and normed <= AT_CMAX-deg45 then
 							local line = clone(min_item.line)
-							if nconn.c ~= inconn.c then
+							if normed ~= AT_CMAX/2 then
 								table.insert(line, minetest.pos_to_string(adj_pos))
 							end
 							table.insert(progress, {
@@ -576,4 +605,322 @@ recalculate_line_to = function(pos_a, pos_b, meta_a, meta_b)
 	end
 	meta_b:set_string("linepath_from_prv", table.concat(line, ";"))
 	return line
+end
+
+-- show found markers when saving object
+
+-- when clicky save, show markers for found blocks
+-- color according to index
+-- add NO RAIL SET if no rail_pos
+-- add markers for path found?
+-- remove markers after minute
+
+local ifilter = function(tbl, fn)
+	local out = {}
+	for i,v in ipairs(tbl) do
+		if fn(v, i, tbl) then
+			table.insert(out)
+		end
+	end
+	return out
+end
+local filter = function(tbl, fn)
+	local out = {}
+	for k,v in pairs(tbl) do
+		if fn(v, k, tbl) then
+			out[k] = v
+		end
+	end
+	return out
+end
+local map = function(tbl, fn)
+	local out = {}
+	for k,v in pairs(tbl) do
+		out[k] = fn(v, k, tbl)
+	end
+	return out
+end
+
+local find_all = function(str, sub)
+	local found = 0
+	local positions = {}
+	while(found)do
+		found = found + 1
+		found = str:find(sub, found)
+		table.insert(positions, found)
+	end
+	return positions
+end
+
+local split = function(str, sep)
+	local seps = find_all(str, sep)
+	local e = 1
+	local out = {}
+	for k,v in pairs(seps) do
+		table.insert(out, string.sub(str, e, v-1))
+		e = v + #sep
+	end
+	table.insert(out, string.sub(str, e, #str))
+	return out
+end
+
+local interpolate = function(a, b, t)
+	return t*(b-a) + a
+end
+
+local RGB = function(r, g, b)
+	-- we have 255 steps per subpixel
+	local base = 0xFF
+
+	-- clamp values, discard fractions
+	r = math.floor(base * math.max(0, math.min(1, r)))
+	g = math.floor(base * math.max(0, math.min(1, g)))
+	b = math.floor(base * math.max(0, math.min(1, b)))
+
+	return r*2^16 + g*2^8 + b
+end
+
+local c_this_path = RGB(.8,.9,.8)
+local c_next_path = RGB(.7,.7,.8)
+local colormethis = function(v, extremes, halfway_point, neighbors)
+	if v.index == nil or v.duplicate_index then
+		return RGB(1,0,1)
+	elseif v.index == halfway_point then
+		return RGB(1,1,1)
+	end
+
+	local a = extremes[1]
+	local b = extremes[2]
+	if v.index < halfway_point then
+		if neighbors ~= nil and neighbors[1] ~= nil then
+			b = neighbors[1].index
+		else
+			b = halfway_point
+		end
+	else
+		if neighbors ~= nil and neighbors[2] ~= nil then
+			a = neighbors[2].index
+		else
+			a = halfway_point
+		end
+	end
+	local t
+	if b - a == 0 then
+		t = 1
+	else
+		t = (v.index - a) / (b - a)
+	end
+
+	-- helper variables
+	local t2 = t^2	-- t squared
+	local it = 1 - t	-- inverse t
+	local it2 = it^2	-- inverse t squared
+
+	local base = .5
+	local r = base
+	local g = base
+	local b = base
+
+	if v.index < halfway_point then
+		--g = g + t2 * (1-base)
+		g = 1
+		t = t2
+	else
+		--b = b + it2 * (1-base)
+		b = 1
+		g = interpolate(g, 1, .4)
+		t = it2
+	end
+
+	local avg = math.sqrt(r^2 + g^2 + b^2)
+	r = interpolate(avg/3*2, r, t)
+	g = interpolate(avg/3*2, g, t)
+	b = interpolate(avg/3*2, b, t)
+
+	if v.duplicate_index then
+		return RGB(1,0,1)
+	elseif not v.has_rail then
+		r = 1
+	elseif not v.has_path then
+		r = interpolate(r, 1, .8)
+	end
+
+	return RGB(r,g,b)
+end
+
+local displayindex = function(v, line)
+	local out = line..": ["..tostring(v.index).."]"
+	if v.index == nil then
+		out = out .. " INVALID INDEX"
+	elseif v.duplicate_index then
+		out = out .. " DUPLICATE INDEX"
+	elseif not v.has_rail then
+		out = out .. " (no rail)"
+	elseif not v.has_path then
+		out = out .. " (no path)"
+	end
+	return out
+end
+
+visualize_to_player = function(pos, meta, neighbors, player, show_path)
+	local name = player:get_player_name()
+	local index = tonumber(meta:get_string("index"))
+	local line = meta:get_string("line")
+
+	local found_blocks = neighbors[3]
+	if markers[name] == nil then
+		markers[name] = {}
+	end
+
+	table.sort(found_blocks, function(a,b) return a.index < b.index end)
+	local idx_extremes = {found_blocks[1].index, found_blocks[#found_blocks].index}
+
+	local new_markers = {}
+	local added_markers = {}
+	local changed_markers = {}
+
+	local done = {}
+	local last_idx = nil
+	for i,v in ipairs(found_blocks) do
+		v.path = v.meta:get_string("linepath_from_prv")
+		v.has_rail = v.rail_pos ~= ""
+		v.has_path = v.path ~= ""
+		v.duplicate_index = v.index ~= nil and v.index == last_idx
+		if v.duplicate_index then
+			found_blocks[i-1].duplicate_index = true
+		end
+		last_idx = v.index
+
+		v.meta = nil
+	end
+	for i,v in ipairs(found_blocks) do
+		local pos_string = minetest.pos_to_string(v.pos)
+		done[pos_string] = v
+		if true then
+		--if markers[name][pos_string] == nil then
+			v.hud_id = player:hud_add({
+				hud_elem_type = "waypoint",
+				name = displayindex(v, line),
+				text = "",
+				number = colormethis(v, idx_extremes, index, neighbors),
+				world_pos = v.pos
+			})
+
+			--added_markers = added_markers + 1
+			table.insert(added_markers, {pos_string, v.hud_id})
+		else
+			v.hud_id = markers[name][pos_string].hud_id
+			player:hud_change(v.hud_id, "name", displayindex(v, line))
+			player:hud_change(v.hud_id, "number", colormethis(v, idx_extremes, index, neighbors))
+
+			--changed_markers = changed_markers + 1
+			table.insert(changed_markers, {pos_string, v.hud_id})
+		end
+		--new_markers = new_markers + 1
+		table.insert(new_markers, {pos_string, v.hud_id})
+	end
+
+	if show_path then
+		local pos_string = minetest.pos_to_string(pos)
+		if done[pos_string].has_path then
+			local path = split(done[pos_string].path, ";")
+			for _,p in ipairs(path) do
+				done[p] = {pos = minetest.string_to_pos(p)}
+				done[p].pos.y = done[p].pos.y - .4
+				if true then
+				--if markers[name][p] == nil then
+					done[p].hud_id = player:hud_add({
+						hud_elem_type = "waypoint",
+						name = "", --"(pPath) | "..tostring(c_this_path),
+						text = "",
+						number = c_this_path,
+						world_pos = done[p].pos
+					})
+
+					--added_markers = added_markers + 1
+					table.insert(added_markers, {p, done[p].hud_id})
+				else
+					done[p].hud_id = markers[name][pos_string].hud_id
+					player:hud_change(done[p].hud_id, "name", "") --"(pPath) | "..tostring(c_this_path))
+					player:hud_change(done[p].hud_id, "number", c_this_path)
+
+					--changed_markers = changed_markers + 1
+					table.insert(changed_markers, {p, done[p].hud_id})
+				end
+				--new_markers = new_markers + 1
+				table.insert(new_markers, {p, done[p].hud_id})
+			end
+		end
+
+		if neighbors[2] ~= nil then
+			pos_string = minetest.pos_to_string(neighbors[2].pos)
+			if done[pos_string].has_path then
+				local path = split(done[pos_string].path, ";")
+				for _,p in ipairs(path) do
+					done[p] = {pos = minetest.string_to_pos(p)}
+					if true then
+					--if markers[name][p] == nil then
+						done[p].hud_id = player:hud_add({
+							hud_elem_type = "waypoint",
+							name = "", --"(nPath) | "..tostring(c_next_path),
+							text = "",
+							number = c_next_path,
+							world_pos = done[p].pos
+						})
+
+						--added_markers = added_markers + 1
+						table.insert(added_markers, {p, done[p].hud_id})
+					else
+						done[p].hud_id = markers[name][pos_string].hud_id
+						player:hud_change(done[p].hud_id, "name", "") --"(nPath) | "..tostring(c_next_path))
+						player:hud_change(done[p].hud_id, "number", c_next_path)
+
+						--changed_markers = changed_markers + 1
+						table.insert(changed_markers, {p, done[p].hud_id})
+					end
+					--new_markers = new_markers + 1
+					table.insert(new_markers, {p, done[p].hud_id})
+				end
+			end
+		end
+	end
+
+	local prev_markers = {}
+	local deleted_markers = {}
+	-- clear vanished elements
+	for p,v in pairs(markers[name]) do
+		--if done[p] == nil then
+			player:hud_remove(v.hud_id)
+			--deleted_markers = deleted_markers + 1
+			table.insert(deleted_markers, {p, v.hud_id})
+		--end
+		--prev_markers = prev_markers + 1
+		table.insert(prev_markers, {p, v.hud_id})
+	end
+
+	--[[
+	for p,v in pairs(markers[name]) do
+		print("old: "..p..": "..v.hud_id)
+	end
+	]]--
+	markers[name] = done
+	--[[
+	for p,v in pairs(markers[name]) do
+		print("new: "..p..": "..v.hud_id)
+	end
+
+	local pme = function(t)
+		t = map(t, function(v) return v[1].." ["..tostring(v[2]).."]" end)
+		table.sort(t)
+		return "{ "..table.concat(t, ", ").." }"
+	end
+
+	minetest.log("error", tostring(#new_markers).." new / "..tostring(#prev_markers).." previous markers" ..
+		" ("..tostring(#added_markers).." added, "..tostring(#changed_markers).." changed, "..tostring(#deleted_markers).." deleted)")
+	minetest.log("error", "new:      "..pme(new_markers))
+	minetest.log("error", "previous: "..pme(prev_markers))
+	minetest.log("error", "added:    "..pme(added_markers))
+	minetest.log("error", "changed:  "..pme(changed_markers))
+	minetest.log("error", "deleted:  "..pme(deleted_markers))
+	]]--
 end
